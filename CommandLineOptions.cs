4f1729d2065e2c8a396d4db19a7f35f67d3602f7
Vratislav Čermák <vratislav.cermak@infor.com>
2023-04-23 09:34:14 +0200

    Initial commit.

---
+++ CommandLineOptions.cs
@@ -0,0 +1,36 @@
+﻿using CommandLine;
+
+namespace PrintThatCard;
+
+internal class CommandLineOptions
+{
+    [Option(shortName: 'l', longName: "lines", Required = false, HelpText = "Print lines.", Default = false)]
+    public bool Lines { get; set; }
+    
+    [Option(shortName: 'c', longName: "columns", Required = false, HelpText = "Columns per page.", Default = 2)]
+    public int Columns { get; set; }
+    
+    [Option(shortName: 'r', longName: "rows", Required = false, HelpText = "Rows per page.", Default = 3)]
+    public int Rows { get; set; }
+    
+    [Option(shortName: 'd', longName: "dpi", Required = false, HelpText = "Dpi of output image.", Default = 300)]
+    public int Dpi { get; set; }
+    
+    [Option(longName: "pagewidth", Required = false, HelpText = "Page width in millimeters.", Default = 210)]
+    public int PageWidth { get; set; }
+    
+    [Option(longName: "pageheight", Required = false, HelpText = "Page height in millimeters.", Default = 297)]
+    public int PageHeight { get; set; }
+    
+    [Option(longName: "pagewidth", Required = false, HelpText = "Card width in millimeters.", Default = 63)]
+    public int CardWidth { get; set; }
+    
+    [Option(longName: "pageheight", Required = false, HelpText = "Card height in millimeters.", Default = 88)]
+    public int CardHeight { get; set; }
+
+    [Value(index: 0, Required = true, HelpText = "Input directory.")]
+    public string Input { get; set; } = string.Empty;
+
+    [Value(index: 1, Required = true, HelpText = "Output file name.")]
+    public string Output { get; set; } = string.Empty;
+}
---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using CommandLine;
+using PrintThatCard;
+
+Parser.Default.ParseArguments<CommandLineOptions>(args)
+    .WithNotParsed(errors =>
+    {
+        Console.WriteLine("Could not parse command line arguments:");
+        
+        foreach (var error in errors)
+        {
+            Console.WriteLine(error.ToString());
+        }
+    })
+    .WithParsed(options =>
+    {
+        float pixelsPerCm = options.Dpi / 2.54f;
+
+        float pageWidthPixels = options.PageWidth / 10f * pixelsPerCm;
+        float pageHeightPixels = options.PageHeight / 10f * pixelsPerCm;
+        
+        using Bitmap bitmap = new Bitmap((int)pageWidthPixels, (int)pageHeightPixels);
+
+        bitmap.SetResolution(options.Dpi, options.Dpi);
+
+        float cardWidthPixels = options.CardWidth/10f * pixelsPerCm;
+        float cardHeightPixels = options.CardHeight/10f * pixelsPerCm;
+
+        float columnWidthPixels = pageWidthPixels / options.Columns;
+        float rowHeightPixels = pageHeightPixels / options.Rows;
+
+        SizeF cellSize = new SizeF(columnWidthPixels, rowHeightPixels);
+
+        using var graphics = Graphics.FromImage(bitmap);
+        Pen pen = new Pen(Color.Black, 1);
+
+        Queue<string> paths = new Queue<string>();
+
+        foreach (var path in Directory.EnumerateFiles(options.Input))
+        {
+            paths.Enqueue(path);
+        }
+
+        for (int column = 0; column < options.Columns; column++)
+        {
+            for (int row = 0; row < options.Rows; row++)
+            {
+                PointF leftTop = new PointF(column * columnWidthPixels, row * rowHeightPixels);
+
+                PointF oneTop = new PointF((cellSize.Width - cardWidthPixels) / 2 + leftTop.X, 0 + leftTop.Y);
+                PointF oneBottom = oneTop with { Y = oneTop.Y + cellSize.Height };
+
+                PointF twoTop = new PointF((cellSize.Width - cardWidthPixels) / 2 + leftTop.X + cardWidthPixels, 0 + leftTop.Y);
+                PointF twoBottom = twoTop with { Y = oneTop.Y + cellSize.Height };
+
+                PointF threeTop = new PointF(0 + leftTop.X, (cellSize.Height - cardHeightPixels) / 2 + leftTop.Y);
+                PointF threeBottom = threeTop with { X = threeTop.X + cellSize.Width };
+
+                PointF fourTop = new PointF(0 + leftTop.X, (cellSize.Height - cardHeightPixels) / 2 + leftTop.Y + cardHeightPixels);
+                PointF fourBottom = fourTop with { X = fourTop.X + cellSize.Width };
+
+                if (options.Lines)
+                {
+                    graphics.DrawLine(pen, oneTop, oneBottom);
+                    graphics.DrawLine(pen, twoTop, twoBottom);
+                    graphics.DrawLine(pen, threeTop, threeBottom);
+                    graphics.DrawLine(pen, fourTop, fourBottom);
+                }
+
+                if (paths.Count > 0)
+                {
+                    Image image = Image.FromFile(paths.Dequeue());
+
+                    graphics.DrawImage(image, oneTop.X, threeTop.Y, cardWidthPixels, cardHeightPixels);
+                }
+            }
+        }
+
+        bitmap.Save(options.Output, ImageFormat.Png);
+    });
